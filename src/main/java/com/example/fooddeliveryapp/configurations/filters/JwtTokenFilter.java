e264b5acf538f732bc56118922caef5ec148a022
Tai Le <leanhtai253@gmail.com>
2023-01-08 16:33:30 +0700

    update packages

---
+++ src/main/java/com/example/fooddeliveryapp/configurations/filters/JwtTokenFilter.java
@@ -0,0 +1,4 @@
+package com.example.fooddeliveryapp.configurations.filters;
+
+public class JwtTokenFilter {
+}
---
+++ src/main/java/com/example/fooddeliveryapp/configurations/mysql/MysqlConfig.java
@@ -0,0 +1,4 @@
+package com.example.fooddeliveryapp.configurations.mysql;
+
+public class MysqlConfig {
+}
