ef3643ea50cd0737a02963c8bed54bcdc0209c0c
Le Tai <leanhtai253@gmail.com>
2023-01-22 11:59:07 +0700

    front end: sign in

--- src/frontend/fooddeliveryapp_fe/askbootstrap.com/preview/osahan-eat/theme-sidebar/js/signin.js
+++ src/frontend/fooddeliveryapp_fe/askbootstrap.com/preview/osahan-eat/theme-sidebar/js/signin.js
@@ -2,14 +2,25 @@ $(document).ready(function(){
 
     $("#btn-signin").click(function(){
         var data = {
-            username: $("#inputEmail").val(),
+            email: $("#inputEmail").val(),
             password: $("#inputPassword").val()
         }
         $.ajax({  
             method: "POST",
-            url: "http://localhost:8080/api/signin",
+            url: "http://localhost:8481/authen/sign-in",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
+            error: function(response) {
+                error = response.responseJSON
+                console.log(error.message)
+                if (Array.isArray(error.message)) {
+                    error.message.forEach(element => {
+                        notify(element);
+                    });
+                } else {
+                    notify(error.message);
+                }
+            }
         }).done(function( result ) {
             if (result.success) {
                 window.location.href = "index.html";
@@ -18,7 +29,16 @@ $(document).ready(function(){
             }
         });
     })
-    
-    
+    $(document).on('click','.notify',function() {
+        $(this).slideUp('fast',function(){$(this).remove();});
+    });
     
 })
+function notify(msg) {
+    $('<p/>').appendTo('#notify').addClass('notify').addClass('text-center').html(msg).slideDown();
+    // $("#notify").append(`
+    //     <p class="text-center", class="notify">
+    //         <a class="text-dark">${msg}</a>
+    //     </p>
+    // `);
+}
