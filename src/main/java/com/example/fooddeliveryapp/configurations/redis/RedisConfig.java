e5e58bd8c36c223705a3bd34bacb68ecefccef06
Le Tai <leanhtai253@gmail.com>
2023-01-30 07:26:59 +0700

    change redis params

--- src/main/java/com/example/fooddeliveryapp/configurations/redis/RedisConfig.java
+++ src/main/java/com/example/fooddeliveryapp/configurations/redis/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.fooddeliveryapp.configurations.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -9,10 +10,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
 
     }
