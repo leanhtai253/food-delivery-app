aac3f61fb3a690b5fce347fd306b6162846dce38
Le Tai <leanhtai253@gmail.com>
2023-01-30 07:31:38 +0700

    deploy config

--- src/main/java/com/example/fooddeliveryapp/configurations/redis/RedisConfig.java
+++ src/main/java/com/example/fooddeliveryapp/configurations/redis/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.fooddeliveryapp.configurations.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -9,10 +10,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+    @Value("${redis.host}")
+    private String redisHost;
 
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Value("${redis.username}")
+    private String redisUsername;
+
+    @Value("${redis.password}")
+    private String redisPassword;
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisStandaloneConfiguration.setUsername(redisUsername);
+        redisStandaloneConfiguration.setPassword(redisPassword);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
 
     }
